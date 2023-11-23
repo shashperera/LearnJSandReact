c1dc7ddce3057ad574f6e1adaf471c3c99b297b8
shashperera <sanduni.p@sliit.lk>
2023-11-23 17:33:04 +0200

    filter,map,sort

--- src/views/Home.js
+++ src/views/Home.js
@@ -14,3 +14,36 @@ const ReactHooks = () => {
 };
 
 export default ReactHooks;
+
+//Create a basic List component
+//use filter,sort and map
+
+//toys
+const toys = [
+    {
+      name: "Chocolate Cake",
+      price: 400,
+      createdAt: "2022-09-01",
+    },
+    {
+      name: "Ice Cream",
+      price: 200,
+      createdAt: "2022-01-02",
+    },
+    {
+      name: "Tiramisu",
+      price: 300,
+      createdAt: "2021-10-03",
+    },
+    {
+      name: "Cheesecake",
+      price: 600,
+      createdAt: "2022-01-04",
+    },
+  ];
+
+//filter toys below 500
+const toysBelow500 = props.data.filter((toys) => {
+    return toys.price < 500;
+})
+console.log(toysBelow500);
--- src/views/chk.js
+++ src/views/chk.js
@@ -1,2 +1,39 @@
-var a = (2, 3, 5);
-console.log(a)
+//Create a basic List component
+//use filter,sort and map
+
+//toys
+const ToysList = [
+    {
+        name: "Chocolate Cake",
+        price: 400,
+        createdAt: "2022-09-01",
+    },
+    {
+        name: "Ice Cream",
+        price: 200,
+        createdAt: "2022-01-02",
+    },
+    {
+        name: "Tiramisu",
+        price: 300,
+        createdAt: "2021-10-03",
+    },
+    {
+        name: "Cheesecake",
+        price: 600,
+        createdAt: "2022-01-04",
+    },
+];
+
+
+
+//filter toys below 500
+const toysBelow500 = ToysList.filter((toys) => {
+    return toys.price < 500;
+})
+
+.sort((a, b) => { 
+    return a.price - b.price; 
+  }) 
+
+console.log(toysBelow500);
